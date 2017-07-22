f34fab5cb00659d7e216cbc7e791b067c32e5317
kernel1 <yubi848484@gmail.com>
2017-07-23 01:30:54 +0900

    initial pusH

---
+++ src/main/java/io/kernel1/blp001/Blproject001Application.java
@@ -0,0 +1,12 @@
+package io.kernel1.blp001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blproject001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Blproject001Application.class, args);
+	}
+}
---
+++ src/test/java/io/kernel1/blp001/Blproject001ApplicationTests.java
@@ -0,0 +1,16 @@
+package io.kernel1.blp001;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Blproject001ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
